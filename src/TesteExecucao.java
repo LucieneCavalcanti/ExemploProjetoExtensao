778ecafdedc9ff2beae2e1e9e299e412355bdd48
LucieneCavalcanti <lucienecavalcanti@live.com>
2025-11-08 14:32:56 -0300

    listar, excluir, editar Status

--- src/TesteExecucao.java
+++ src/TesteExecucao.java
@@ -7,6 +7,8 @@ import model.Funcionario;
 import model.Status;
 import model.Usuario;
 
+
+
 public class TesteExecucao {
 public static void main(String[] args) {
     //Usuario u1 = new Usuario(); // classe é abstract
@@ -17,23 +19,80 @@ public static void main(String[] args) {
     // System.out.println("Nome do cliente: " + c1.getNome());
 
    // Conexao con = new Conexao();
-   StatusData DAO = new StatusData();
-   Status s1 = new Status();
-   Scanner ler = new Scanner(System.in);
-   System.out.println("Digite a descrição do status:"); 
-   s1.setDescricao(ler.next());
-   if(DAO.incluir(s1)){
-       System.out.println("Status incluído com sucesso!");
-   } else {
-       System.out.println("Erro ao incluir status.");
-   }
+//    StatusData DAO = new StatusData();
+//    Status s1 = new Status();
+//    Scanner ler = new Scanner(System.in);
+//    System.out.println("Digite a descrição do status:"); 
+//    s1.setDescricao(ler.next());
+//    if(DAO.incluir(s1)){
+//        System.out.println("Status incluído com sucesso!");
+//    } else {
+//        System.out.println("Erro ao incluir status.");
+//    }
 
-   System.out.println("Digite o ID do status a ser excluído:");
-    int idExcluir = ler.nextInt();
-    if(DAO.excluir(idExcluir)){
-        System.out.println("Status excluído com sucesso!");
-    } else {
-        System.out.println("Erro ao excluir status.");
-    }
+//    System.out.println("Digite o ID do status a ser excluído:");
+//     int idExcluir = ler.nextInt();
+//     if(DAO.excluir(idExcluir)){
+//         System.out.println("Status excluído com sucesso!");
+//     } else {
+//         System.out.println("Erro ao excluir status.");
+//     }
+//MENU
+    int opcao = 0;
+    do{
+        System.out.println("---- MENU PRINCIPAL ---- ");
+        System.out.println("-- STATUS");
+        System.out.println("11 - Cadastrar novo status");
+        System.out.println("12 - Excluir status");
+        System.out.println("13 - Listar todos os status");
+        System.out.println("14 - Consultar status por ID");
+        System.out.println("15 - Editar status");
+        System.out.println("------------------------");
+        System.out.println("2 - CLIENTE");
+        System.out.println("3 - FUNCIONÁRIO");
+        System.out.println("4 - USUÁRIO");
+        System.out.println("5 - PRODUTO");  
+        System.out.println("6 - PEDIDO");
+        System.out.println("7 - SAIR");
+        System.out.print("Escolha uma opção: ");
+        Scanner ler = new Scanner(System.in);
+        opcao = ler.nextInt();
+        switch (opcao) {
+            case 11:
+                Status status = new Status();
+                System.out.println("---- CADASTRO DE STATUS ---- ");
+                System.out.print("Digite a descrição do status: ");
+                String descricao = ler.next();
+                status.setDescricao(descricao);
+                StatusData statusData = new StatusData();
+                if(statusData.incluir(status)){
+                    System.out.println("Status incluído com sucesso!");
+                } else {
+                    System.out.println("Erro ao incluir status.");
+                }
+                break;
+            case 12:
+                System.out.println("---- EXCLUSÃO DE STATUS ---- ");
+                System.out.print("Digite o ID do status a ser excluído: ");
+                int idExcluir = ler.nextInt();
+                StatusData statusDataExcluir = new StatusData();
+                if(statusDataExcluir.excluir(idExcluir)){
+                    System.out.println("Status excluído com sucesso!");
+                } else {
+                    System.out.println("Erro ao excluir status.");
+                }
+                break;
+            case 13:
+                System.out.println("---- LISTA DE STATUS ---- ");
+                StatusData statusDataListar = new StatusData();
+                for(Status s : statusDataListar.listar()){
+                    System.out.println("ID: " + s.getId() + 
+                     " - Descrição:" + s.getDescricao());
+                }
+                break;
+            default:
+                break;
+        }
+    } while (opcao!=7);
 }
 }
--- src/data/StatusData.java
+++ src/data/StatusData.java
@@ -1,6 +1,8 @@
 package data;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import model.Status;
 
@@ -22,7 +24,20 @@ public class StatusData extends Conexao { //implements Crud {
         return true;
     }
 
-    public boolean alterar(Object obj) {
+    public boolean alterar(Status obj) {
+        String sql = "update tbStatus set descricao=? where id=?";
+        try {
+            PreparedStatement ps = getConnection().prepareStatement(sql);
+            ps.setString(1, obj.getDescricao());
+            ps.setInt(2, obj.getId());
+            //var ps = getConnection().prepareStatement(sql);
+            //ps.setString(1, ((Status)obj).getDescricao());
+            ps.executeUpdate();
+            ps.close();
+        } catch (Exception e) {
+            System.out.println("Erro ao incluir status: " + e.getMessage());
+            return false;
+        }
         return true;
     }
 
@@ -41,6 +56,24 @@ public class StatusData extends Conexao { //implements Crud {
         }
        return true;
     }
-
+    public ArrayList<Status> listar() {
+        ArrayList<Status> lista = new ArrayList<>();
+        String sql = "SELECT * FROM tbStatus order by descricao";
+        try {
+            PreparedStatement ps = getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Status status = new Status();
+                status.setId(rs.getInt("id"));
+                status.setDescricao(rs.getString("descricao"));
+                lista.add(status);
+            }
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            System.out.println("Erro ao listar status: " + e.getMessage());
+        }
+        return lista;
+    }
 
 }
